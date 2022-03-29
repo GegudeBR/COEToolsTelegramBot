0ea8f72b714bfb7cd5f66916da1d1ca32fcaa343
George <67044516+GegudeBR@users.noreply.github.com>
2022-03-28 17:55:36 -0700

    Adding telegram bot

---
+++ Laps.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self, laps):
+        self.laps = laps
+
+    def on_modified(self, event):
+        #print(f'Event type: {event.event_type}  path : {event.src_path}')
+
+        if event.src_path == self.laps.out_path:
+            time.sleep(0.1)
+            f = open(self.laps.out_path, "r")
+            self.laps.password = f.readline()
+            f.close()
+            self.laps.observer.stop()
+        
+
+class LAPS():
+    def __init__(self, update):
+        self.password = None
+        self.update = update
+        self.in_path = "C:/temp/in.laps"
+        self.out_path = "C:/temp/out.laps"
+        self.event_handler = MyHandler(self)
+        self.observer = Observer()
+        self.observer.schedule(self.event_handler, path='C:/temp/', recursive=False)
+        self.observer.start()
+
+
+    def start(self, computer_name):
+        self.computer_name = computer_name
+        f = open(self.in_path, "w")
+        f.write(self.computer_name)
+        f.close()
